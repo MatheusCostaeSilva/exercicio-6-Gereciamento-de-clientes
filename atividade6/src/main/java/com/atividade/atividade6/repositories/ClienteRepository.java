4303f33609281e484f7e499e1e1b06438a9bf381
unknown <matheuscostaesilva12@gmail.com>
2026-03-18 19:34:13 -0300

    model criada

---
+++ atividade6/src/main/java/com/atividade/atividade6/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.atividade.atividade6.repositories;
+
+import com.atividade.atividade6.models.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+}
